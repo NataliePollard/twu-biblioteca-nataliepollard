da901c2eeef85642e13f00a2f04253eb2182a3c8
Natalie <natalie.pollard@richmond.edu>
2015-12-20 21:21:21 -0700

    Added listbook functionality

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,12 +4,15 @@ import java.util.LinkedList;
 
 
 public class BibliotecaApp {
-
+    private static LinkedList books;
 
     public static void main(String[] args) {
+        books = new LinkedList();
+        books.add("Harry Potter");
+        books.add("Lord of the Rings");
+
         System.out.println("Welcome to Biblioteca!");
+        System.out.println(books);
     }
-
-
-
+    
 }
